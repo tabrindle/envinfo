389199a0659940a0942781829beb1473b081665a
Trevor Brindle <tabrindle@gmail.com>
2018-01-23 15:26:42 -0500

    fix: no options object throws error

--- src/envinfo.js
+++ src/envinfo.js
@@ -6,7 +6,8 @@ var helpers = require('./helpers');
 var copypasta = require('clipboardy');
 
 module.exports.helpers = helpers;
-module.exports.print = function print(options) {
+module.exports.print = function print(opts) {
+  var options = opts || {};
   var log = [];
 
   log.push('');
@@ -21,94 +22,92 @@ module.exports.print = function print(options) {
   if (!options.noNativeIDE) log.push('  Android Studio: ' + helpers.getAndroidStudioVersion());
   log.push('');
 
-  if (options) {
-    if (options.browsers && process.platform === 'darwin') {
-      options.clipboard ? log.push('Browsers:') : log.push('\x1b[4mBrowsers:\x1b[0m');
-      var browsers = helpers.browserBundleIdentifiers;
-      var browserVersions = Object.keys(browsers).map(function browserMap(browser) {
-        return '  ' + browser + ': ' + helpers.getDarwinApplicationVersion(browsers[browser]);
-      });
-      log = log.concat(browserVersions);
+  if (options.browsers && process.platform === 'darwin') {
+    options.clipboard ? log.push('Browsers:') : log.push('\x1b[4mBrowsers:\x1b[0m');
+    var browsers = helpers.browserBundleIdentifiers;
+    var browserVersions = Object.keys(browsers).map(function browserMap(browser) {
+      return '  ' + browser + ': ' + helpers.getDarwinApplicationVersion(browsers[browser]);
+    });
+    log = log.concat(browserVersions);
+    log.push('');
+  }
+
+  if (options.packages) {
+    var packageJson = helpers.getPackageJsonByPath('/package.json');
+
+    if (!packageJson) {
+      log.push('ERROR: package.json not found!');
       log.push('');
+      log = log.join('\n');
+      if (options.clipboard) copypasta.copy(log);
+      console.log(log);
+      return;
     }
 
-    if (options.packages) {
-      var packageJson = helpers.getPackageJsonByPath('/package.json');
+    options.clipboard
+      ? log.push('Packages: (wanted => installed)')
+      : log.push('\x1b[4mPackages:\x1b[0m (wanted => installed)');
 
-      if (!packageJson) {
-        log.push('ERROR: package.json not found!');
-        log.push('');
-        log = log.join('\n');
-        if (options.clipboard) copypasta.copy(log);
-        console.log(log);
-        return;
-      }
+    var devDependencies = packageJson.devDependencies || {};
+    var dependencies = packageJson.dependencies || {};
+    var topLevelDependencies = Object.assign({}, devDependencies, dependencies);
+    var packagePaths;
 
-      options.clipboard
-        ? log.push('Packages: (wanted => installed)')
-        : log.push('\x1b[4mPackages:\x1b[0m (wanted => installed)');
-
-      var devDependencies = packageJson.devDependencies || {};
-      var dependencies = packageJson.dependencies || {};
-      var topLevelDependencies = Object.assign({}, devDependencies, dependencies);
-      var packagePaths;
-
-      if (options.duplicates || options.fullTree) {
-        packagePaths = helpers.getAllPackageJsonPaths();
-      }
-
-      var logFunction = function logFunc(dep) {
-        var trimmedDep = dep.trim();
-        var wanted = topLevelDependencies[trimmedDep] || '';
-        var dependencyPackageJson = helpers.getPackageJsonByName(dep);
-        var installed = dependencyPackageJson ? dependencyPackageJson.version : 'Not Found';
-        var duplicates = '';
-
-        if (options.duplicates) {
-          duplicates = helpers
-            .getModuleVersions(dep, packagePaths)
-            .filter(depVer => depVer !== installed)
-            .join(', ');
-          if (duplicates) duplicates = ' (' + duplicates + ')';
-        }
-
-        if (wanted) wanted += ' => ';
-
-        log.push('  ' + trimmedDep + ': ' + wanted + installed + duplicates);
-      };
-
-      if (Array.isArray(options.packages)) {
-        options.packages.map(logFunction);
-      } else if (typeof options.packages === 'string') {
-        options.packages.split(',').map(logFunction);
-      } else if (typeof options.packages === 'boolean') {
-        if (options.fullTree) {
-          var allDependencies = packagePaths
-            .map(filePath => {
-              var json = helpers.getPackageJsonByPath(filePath);
-              return {
-                name: json.name,
-                version: json.version,
-              };
-            })
-            .reduce((acc, val) => {
-              return Object.assign(acc, {
-                [val.name]: acc[val.name] ? acc[val.name].concat([val.version]) : [val.version],
-              });
-            }, {});
-          Object.keys(allDependencies).map(logFunction);
-        } else {
-          Object.keys(topLevelDependencies).map(logFunction);
-        }
-      }
-
-      log.push('');
+    if (options.duplicates || options.fullTree) {
+      packagePaths = helpers.getAllPackageJsonPaths();
     }
-    log = log.join('\n');
 
-    if (options.clipboard) {
-      copypasta.writeSync(log);
+    var logFunction = function logFunc(dep) {
+      var trimmedDep = dep.trim();
+      var wanted = topLevelDependencies[trimmedDep] || '';
+      var dependencyPackageJson = helpers.getPackageJsonByName(dep);
+      var installed = dependencyPackageJson ? dependencyPackageJson.version : 'Not Found';
+      var duplicates = '';
+
+      if (options.duplicates) {
+        duplicates = helpers
+          .getModuleVersions(dep, packagePaths)
+          .filter(depVer => depVer !== installed)
+          .join(', ');
+        if (duplicates) duplicates = ' (' + duplicates + ')';
+      }
+
+      if (wanted) wanted += ' => ';
+
+      log.push('  ' + trimmedDep + ': ' + wanted + installed + duplicates);
+    };
+
+    if (Array.isArray(options.packages)) {
+      options.packages.map(logFunction);
+    } else if (typeof options.packages === 'string') {
+      options.packages.split(',').map(logFunction);
+    } else if (typeof options.packages === 'boolean') {
+      if (options.fullTree) {
+        var allDependencies = packagePaths
+          .map(filePath => {
+            var json = helpers.getPackageJsonByPath(filePath);
+            return {
+              name: json.name,
+              version: json.version,
+            };
+          })
+          .reduce((acc, val) => {
+            return Object.assign(acc, {
+              [val.name]: acc[val.name] ? acc[val.name].concat([val.version]) : [val.version],
+            });
+          }, {});
+        Object.keys(allDependencies).map(logFunction);
+      } else {
+        Object.keys(topLevelDependencies).map(logFunction);
+      }
     }
+
+    log.push('');
+  }
+  log = log.join('\n');
+
+  if (options.clipboard) {
+    copypasta.writeSync(log);
   }
 
   console.log(log);
