77dbd3e84d8b2a6c67479351d6ac97d5665769bf
Trevor Brindle <tabrindle@gmail.com>
2018-03-10 11:26:05 -0500

    feat: add react-native preset

--- src/presets.js
+++ src/presets.js
@@ -4,4 +4,12 @@ module.exports = {
     Binaries: ['Node', 'Yarn', 'npm'],
     npmPackages: ['jest'],
   },
+  'react-native': {
+    System: ['OS', 'CPU'],
+    Binaries: ['Node', 'Yarn', 'npm', 'Watchman'],
+    SDKs: ['iOS', 'Android'],
+    IDEs: ['Android Studio', 'Xcode'],
+    npmPackages: ['react', 'react-native'],
+    npmGlobalPackages: ['react-native-cli'],
+  },
 };
