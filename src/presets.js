01e549fa18fd60f2988918f1396ccf8179cc85b1
Trevor Brindle <tabrindle@gmail.com>
2018-03-11 11:56:34 -0400

    add webpack preset

--- src/presets.js
+++ src/presets.js
@@ -12,4 +12,10 @@ module.exports = {
     npmPackages: ['react', 'react-native'],
     npmGlobalPackages: ['react-native-cli'],
   },
+  webpack: {
+    System: ['OS', 'CPU'],
+    Binaries: ['Node', 'Yarn', 'npm'],
+    npmPackages: ['webpack', 'webpack-cli'],
+    npmGlobalPackages: ['webpack', 'webpack-cli'],
+  },
 };
