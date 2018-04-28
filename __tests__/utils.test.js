7a898399deabe5075df8b3a8063860f2d2365495
Trevor Brindle <tabrindle@gmail.com>
2018-04-28 11:18:37 -0400

    feat: added db helpers

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,10 +1,5 @@
 const utils = require('../src/utils');
 const cases = {
-  mysql: {
-    index: 1,
-    string: 'mysql  Ver 14.14 Distrib 5.7.21, for osx10.13 (x86_64) using  EditLine wrapper',
-    version: '5.7.21',
-  },
   bash: {
     string: 'GNU bash, version 4.4.12(1)-release (x86_64-apple-darwin17.0.0)',
     version: '4.4.12',
@@ -24,18 +19,43 @@ const cases = {
   Elixir 1.6.2 (compiled with OTP 20)`,
     version: '1.6.2',
   },
-  sublime: {
-    regex: /\d+/,
-    string: 'Sublime Text Build 3143',
-    version: '3143',
-  },
   go: {
     string: 'go version go1.9.3 darwin/amd64',
     version: '1.9.3',
   },
+  mariadb: {
+    index: 1,
+    string: 'mysql  Ver 15.1 Distrib 10.2.14-MariaDB, for osx10.13 (x86_64) using readline 5.1',
+    version: '10.2.14',
+  },
+  mongodb: {
+    string: `MongoDB shell version v3.6.4
+    git version: d0181a711f7e7f39e60b5aeb1dc7097bf6ae5856
+    OpenSSL version: OpenSSL 1.0.2o  27 Mar 2018`,
+    version: '3.6.4',
+  },
+  mysql: {
+    index: 1,
+    string: 'mysql  Ver 14.14 Distrib 5.7.21, for osx10.13 (x86_64) using  EditLine wrapper',
+    version: '5.7.21',
+  },
+  postgres: {
+    string: 'postgres (PostgreSQL) 10.3',
+    version: 10.3,
+  },
   ruby: {
     string: 'ruby 2.3.3p222 (2016-11-21 revision 56859) [universal.x86_64-darwin17]',
     version: '2.3.3',
+  },
+  sqlite: {
+    string:
+      '3.19.4 2017-08-18 19:28:12 605907e73adb4533b12d22be8422f17a8dc125b5c37bb391756a11fc3a8c4d10',
+    version: '3.19.4',
+  },
+  sublime: {
+    regex: /\d+/,
+    string: 'Sublime Text Build 3143',
+    version: '3143',
   },
   virtualbox: {
     string: '5.2.8r121009',
--- src/helpers.js
+++ src/helpers.js
@@ -103,6 +103,40 @@ module.exports = Object.assign({}, utils, packages, {
     );
   },
 
+  getMySQLInfo: () => {
+    utils.log('trace', 'getMySQLInfo');
+    return Promise.all([
+      utils
+        .run('mysql --version')
+        .then(v => `${utils.findVersion(v, null, 1)}${v.includes('MariaDB') ? ' (MariaDB)' : ''}`),
+      utils.which('mysql'),
+    ]).then(v => utils.determineFound('MySQL', v[0], v[1]));
+  },
+
+  getMongoDBInfo: () => {
+    utils.log('trace', 'getMongoDBInfo');
+    return Promise.all([
+      utils.run('mongo --version').then(utils.findVersion),
+      utils.which('mongo'),
+    ]).then(v => utils.determineFound('MongoDB', v[0], v[1]));
+  },
+
+  getSQLiteInfo: () => {
+    utils.log('trace', 'getSQLiteInfo');
+    return Promise.all([
+      utils.run('sqlite3 --version').then(utils.findVersion),
+      utils.which('sqlite3'),
+    ]).then(v => utils.determineFound('SQLite', v[0], v[1]));
+  },
+
+  getPostgreSQLInfo: () => {
+    utils.log('trace', 'getPostgreSQLInfo');
+    return Promise.all([
+      utils.run('postgres --version').then(utils.findVersion),
+      utils.which('postgres'),
+    ]).then(v => utils.determineFound('PostgreSQL', v[0], v[1]));
+  },
+
   getCPUInfo: () => {
     utils.log('trace', 'getCPUInfo');
     let info;
