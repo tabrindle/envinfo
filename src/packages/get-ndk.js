1ece2f0db5a0305fa3eb9240523b6397374815d8
gengjiawen <technicalcute@gmail.com>
2018-12-15 18:33:40 +0800

    fix lint problem
    Signed-off-by: gengjiawen <technicalcute@gmail.com>

--- src/packages/get-ndk.js
+++ src/packages/get-ndk.js
@@ -1,25 +1,13 @@
 const fs = require('fs');
 const path = require('path');
 
-function getNdk() {
-  if (process.env.ANDROID_NDK) {
-    return getNdkVersionFromPath(process.env.ANDROID_NDK)
-  }
-
-  if (process.env.ANDROID_HOME) {
-    return getNdkVersionFromPath(path.join(process.env.ANDROID_HOME, 'ndk-bundle'));
-  }
-
-  return undefined
-}
-
 function getNdkVersionFromPath(ndkDir) {
-  const metaPath = path.join(ndkDir, 'source.properties')
+  const metaPath = path.join(ndkDir, 'source.properties');
   if (fs.existsSync(metaPath)) {
     const contents = fs.readFileSync(metaPath).toString();
-    const split = contents.split("\n");
-    for (const i of split) {
-      const splits = i.split('=');
+    const split = contents.split('\n');
+    for (let i = 0; i < split.length; i += 1) {
+      const splits = split[i].split('=');
       if (splits.length === 2) {
         if (splits[0].trim() === 'Pkg.Revision') {
           return splits[1].trim();
@@ -28,7 +16,19 @@ function getNdkVersionFromPath(ndkDir) {
     }
   }
 
-  return undefined
+  return undefined;
 }
 
-exports.getNdk = getNdk
+function getNdk() {
+  if (process.env.ANDROID_NDK) {
+    return getNdkVersionFromPath(process.env.ANDROID_NDK);
+  }
+
+  if (process.env.ANDROID_HOME) {
+    return getNdkVersionFromPath(path.join(process.env.ANDROID_HOME, 'ndk-bundle'));
+  }
+
+  return undefined;
+}
+
+exports.getNdk = getNdk;
