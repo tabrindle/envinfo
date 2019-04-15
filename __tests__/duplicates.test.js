9a12ed6d87d11155b13d3d07e51f08b95065ba1c
Chen Gang <gangc.cxy@foxmail.com>
2019-04-15 23:44:44 +0800

    fix: wrong duplicates (#95)

---
+++ __tests__/duplicates.test.js
@@ -0,0 +1,26 @@
+const envinfo = require('../src/envinfo');
+const path = require('path');
+
+describe('Running the programmatic interface', () => {
+  test('return expected duplicates in json', async () => {
+    const cwd = process.cwd();
+    process.chdir(path.join(__dirname, 'packages/test-duplicates'));
+    try {
+      const data = await envinfo.run({ npmPackages: ['b'] }, { duplicates: true, json: true });
+      expect(JSON.parse(data)).toMatchSnapshot();
+    } finally {
+      process.chdir(cwd);
+    }
+  });
+
+  test('return expected duplicates in yaml', async () => {
+    const cwd = process.cwd();
+    process.chdir(path.join(__dirname, 'packages/test-duplicates'));
+    try {
+      const data = await envinfo.run({ npmPackages: ['b'] }, { duplicates: true });
+      expect(data).toMatchSnapshot();
+    } finally {
+      process.chdir(cwd);
+    }
+  });
+});
