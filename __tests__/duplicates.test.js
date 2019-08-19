2a9c1534f48518e97662fde097337fefe98df980
Xiaoli <544028951@qq.com>
2019-08-20 06:40:53 +0800

    fix: prevent getting no npmPackages on windows (#114)

--- __tests__/duplicates.test.js
+++ __tests__/duplicates.test.js
@@ -4,7 +4,7 @@ const path = require('path');
 describe('Running the programmatic interface', () => {
   test('return expected duplicates in json', async () => {
     const cwd = process.cwd();
-    process.chdir(path.join(__dirname, 'packages/test-duplicates'));
+    process.chdir(path.join(__dirname, 'packages', path.sep, 'test-duplicates'));
     try {
       const data = await envinfo.run({ npmPackages: ['b'] }, { duplicates: true, json: true });
       expect(JSON.parse(data)).toMatchSnapshot();
@@ -15,7 +15,7 @@ describe('Running the programmatic interface', () => {
 
   test('return expected duplicates in yaml', async () => {
     const cwd = process.cwd();
-    process.chdir(path.join(__dirname, 'packages/test-duplicates'));
+    process.chdir(path.join(__dirname, 'packages', path.sep, 'test-duplicates'));
     try {
       const data = await envinfo.run({ npmPackages: ['b'] }, { duplicates: true });
       expect(data).toMatchSnapshot();
