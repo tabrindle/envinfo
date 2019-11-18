f2d3a21a8da94576606ba2e87c088183529d9f0f
Jiawen Geng <technicalcute@gmail.com>
2019-11-18 23:20:17 +0800

    fix ci problem (#131)

--- src/helpers/languages.js
+++ src/helpers/languages.js
@@ -21,9 +21,10 @@ module.exports = {
 
   getGoInfo: () => {
     utils.log('trace', 'getGoInfo');
-    return Promise.all([utils.run('go version').then(utils.findVersion), utils.which('go')]).then(
-      v => utils.determineFound('Go', v[0], v[1])
-    );
+    return Promise.all([
+      utils.run('go version').then(utils.findVersion),
+      utils.which('go'),
+    ]).then(v => utils.determineFound('Go', v[0], v[1]));
   },
 
   getJavaInfo: () => {
@@ -38,9 +39,10 @@ module.exports = {
 
   getPerlInfo: () => {
     utils.log('trace', 'getPerlInfo');
-    return Promise.all([utils.run('perl -v').then(utils.findVersion), utils.which('perl')]).then(
-      v => utils.determineFound('Perl', v[0], v[1])
-    );
+    return Promise.all([
+      utils.run('perl -v').then(utils.findVersion),
+      utils.which('perl'),
+    ]).then(v => utils.determineFound('Perl', v[0], v[1]));
   },
 
   getPHPInfo: () => {
@@ -76,9 +78,10 @@ module.exports = {
 
   getRubyInfo: () => {
     utils.log('trace', 'getRubyInfo');
-    return Promise.all([utils.run('ruby -v').then(utils.findVersion), utils.which('ruby')]).then(
-      v => utils.determineFound('Ruby', v[0], v[1])
-    );
+    return Promise.all([
+      utils.run('ruby -v').then(utils.findVersion),
+      utils.which('ruby'),
+    ]).then(v => utils.determineFound('Ruby', v[0], v[1]));
   },
 
   getRustInfo: () => {
