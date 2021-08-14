c9d53b354d9f642f87e1d69f2678f85fb4548ca2
Ross Wollman <rwoll@users.noreply.github.com>
2021-08-14 16:21:15 -0700

    preset(playwright): add new packages
    Playwright now has some additional supporting packages published under the `@playwright/*` npm scope (e.g. https://www.npmjs.com/package/@playwright/test from the docs https://playwright.dev/docs/api/class-test).

--- src/presets.js
+++ src/presets.js
@@ -146,6 +146,6 @@ module.exports = {
     System: ['OS', 'Memory', 'Container'],
     Binaries: ['Node', 'Yarn', 'npm'],
     Languages: ['Bash'],
-    npmPackages: 'playwright*',
+    npmPackages: '{playwright*,@playwright/*}',
   },
 };
