9ca92b281652ca0c973a493c2fdd08ac0c15d1e8
Harry Gwinnell <harry@harrygwinnell.co.uk>
2021-12-07 09:22:34 +0000

    chore: add exclusion for os-name transform

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,6 +1,9 @@
+const esModules = ['os-name'].join('|');
+
 module.exports = {
     transform: {
       "^.+\\.(js|jsx)$": "babel-jest",
     },
-    testEnvironment: "node"
+    testEnvironment: "node",
+    transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
   };
--- webpack.config.cjs
+++ webpack.config.cjs
@@ -24,7 +24,7 @@ module.exports = {
     rules: [
       {
         use: 'babel-loader',
-        exclude: /(node_modules)/,
+        exclude: /node_modules/,
         test: /\.js$/,
       },
     ],
