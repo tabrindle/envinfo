c3dddb8c0a4ece884abd37276eb23fd05fbe582c
Harry Gwinnell <harry@harrygwinnell.co.uk>
2021-12-07 09:41:49 +0000

    chore: update Jest config to support transforming other modules

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,4 +1,4 @@
-const esModules = ['os-name'].join('|');
+const esModules = ['os-name', 'macos-release', 'windows-release'].join('|');
 
 module.exports = {
     transform: {
@@ -6,4 +6,5 @@ module.exports = {
     },
     testEnvironment: "node",
     transformIgnorePatterns: [`/node_modules/(?!${esModules})`],
+    setupFilesAfterEnv: ["./setupTests.cjs"]
   };
