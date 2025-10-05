9a8d8919accfe7f9b69210ec672986d1e432d891
Jiawen Geng <technicalcute@gmail.com>
2025-10-05 02:23:59 -0700

    fix: make windows android sdk detect more robust

--- src/helpers/sdks.js
+++ src/helpers/sdks.js
@@ -1,3 +1,4 @@
+const os = require('os');
 const fs = require('fs');
 const path = require('path');
 const utils = require('../utils');
@@ -26,6 +27,13 @@ module.exports = {
       .then(output => {
         if (!output && utils.isMacOS)
           return utils.run('~/Library/Android/sdk/tools/bin/sdkmanager --list');
+        return output;
+      })
+      .then(output => {
+        if (!output && utils.isWindows) {
+          const sdk_path = path.join(os.homedir(), 'AppData/Local/Android/Sdk/cmdline-tools/latest/bin/sdkmanager')
+          return utils.run(`${sdk_path} --list`);
+        }
         return output;
       })
       .then(output => {
