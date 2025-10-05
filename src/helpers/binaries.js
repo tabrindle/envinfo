553603bac227da5ac93017b6394134ba4f53e96e
gengjiawen <technicalcute@gmail.com>
2025-10-05 20:47:16 +0800

    fix: windows path issue

--- src/helpers/binaries.js
+++ src/helpers/binaries.js
@@ -10,13 +10,13 @@ module.exports = {
             .which('node')
             .then(nodePath => (nodePath ? utils.run(nodePath + ' -v') : Promise.resolve('')))
             .then(utils.findVersion),
-      utils.which('node').then(utils.condensePath),
+      utils.which('node'),
     ]).then(v => utils.determineFound('Node', v[0], v[1]));
   },
 
   getnpmInfo: () => {
     utils.log('trace', 'getnpmInfo');
-    return Promise.all([utils.run('npm -v'), utils.which('npm').then(utils.condensePath)]).then(v =>
+    return Promise.all([utils.run('npm -v'), utils.which('npm')]).then(v =>
       utils.determineFound('npm', v[0], v[1])
     );
   },
@@ -33,23 +33,21 @@ module.exports = {
 
   getYarnInfo: () => {
     utils.log('trace', 'getYarnInfo');
-    return Promise.all([
-      utils.run('yarn -v'),
-      utils.which('yarn').then(utils.condensePath),
-    ]).then(v => utils.determineFound('Yarn', v[0], v[1]));
+    return Promise.all([utils.run('yarn -v'), utils.which('yarn')]).then(v =>
+      utils.determineFound('Yarn', v[0], v[1])
+    );
   },
 
   getpnpmInfo: () => {
     utils.log('trace', 'getpnpmInfo');
-    return Promise.all([
-      utils.run('pnpm -v'),
-      utils.which('pnpm').then(utils.condensePath),
-    ]).then(v => utils.determineFound('pnpm', v[0], v[1]));
+    return Promise.all([utils.run('pnpm -v'), utils.which('pnpm')]).then(v =>
+      utils.determineFound('pnpm', v[0], v[1])
+    );
   },
 
   getbunInfo: () => {
     utils.log('trace', 'getbunInfo');
-    return Promise.all([utils.run('bun -v'), utils.which('bun').then(utils.condensePath)]).then(v =>
+    return Promise.all([utils.run('bun -v'), utils.which('bun')]).then(v =>
       utils.determineFound('bun', v[0], v[1])
     );
   },
@@ -63,7 +61,7 @@ module.exports = {
             .which('deno')
             .then(denoPath => (denoPath ? utils.run(denoPath + ' --version') : Promise.resolve('')))
             .then(utils.findVersion),
-      utils.which('deno').then(utils.condensePath),
+      utils.which('deno'),
     ]).then(v => utils.determineFound('Deno', v[0], v[1]));
   },
 };
